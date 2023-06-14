f3088d35aa9a911df285d1bc6e3a1b1a9f5c8242
votanbinn <135816354+votanbinn@users.noreply.github.com>
2023-06-14 15:02:30 +0700

    Add files via upload

---
+++ tinhtoan2soo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int cong(int a,int b)
+{
+	return a+b;
+}
+
+int tru(int a, int b)
+{
+	return a-b;
+}
+
+int nhan(int a, int b)
+{
+	return a*b;
+}
+int chia(int a,int b){
+	return a/b;
+}
+int mu(int a,int b){
+	return pow(a,b);
+}
+main()
+{
+	
+}
